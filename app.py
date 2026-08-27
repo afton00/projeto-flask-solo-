35d12fc19b75eb7c2911f3c5a96cc8bbfd0ba0d1
afton00 <136352772+afton00@users.noreply.github.com>
2026-08-27 14:37:15 +0000

    Atualiza app Flask

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return """
-    <h1>Projeto Flask da Dupla</h1>
-    <p>Nosso primeiro projeto com Flask, Git e GitHub!</p>
-    """
-
+    return """
+<h1>Projeto Flask da Dupla</h1>
+<p>Nosso primeiro projeto com Flask, Git e GitHub!</p>
+<p>Projeto desenvolvido por nossa dupla.</p>
+"""
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
